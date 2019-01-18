995186b93f9a896337c7820ef85bad8512dddf81
Neil Scheuermann <neilscheuermann@gmail.com>
2019-01-18 10:13:14 -0700

    moved message creation logic into a utility function

--- server.js
+++ server.js
@@ -1,105 +1,62 @@
-const axios = require('axios')
-const cron = require('node-cron')
-const secrets = require('./secrets')
-const twilio = require('twilio')
+const axios = require('axios');
+const cron = require('node-cron');
+const secrets = require('./secrets');
+const twilio = require('twilio');
+const {
+  SPORTS_FEEDS_API_KEY,
+  SPORTS_FEEDS_PASSWORD,
+  MY_NUMBER,
+  TWILIO_NUMBER,
+} = secrets;
+const createMessagesForLiveGames = require('./utilityFunctions');
 
-const twilioClient = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+const twilioClient = new twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
-const sportsFeedsApikey = secrets.API_KEY
-const sportsFeedsPassword = secrets.PASSWORD
-
-const encryptedAPIKey = Buffer.from(sportsFeedsApikey + ':' + sportsFeedsPassword).toString('base64')
+const mySportsFeedsEncryption = Buffer.from(
+  SPORTS_FEEDS_API_KEY + ':' + SPORTS_FEEDS_PASSWORD
+).toString('base64');
 
 /*
 played status is either UNPLAYED, LIVE, COMPLETED_PENDING_REVIEW, or COMLETED
 */
 
-const todaysDate = new Date();
-console.log('todays date: ', todaysDate)
-
-const todaysGames = async () => {
+const todaysGames = async date => {
   try {
+    // Pulls a JSON file of most up-to-date live data for all games for the requested date.
     const response = await axios({
-      type: "GET",
-      url: `https://api.mysportsfeeds.com/v2.0/pull/nba/2018-2019-regular/date/20190117/games.json`,
+      type: 'GET',
+      url: `https://api.mysportsfeeds.com/v2.0/pull/nba/2018-2019-regular/date/${date}/games.json`,
       dataType: 'json',
       headers: {
-        "Authorization": "Basic " + encryptedAPIKey
+        Authorization: 'Basic ' + mySportsFeedsEncryption,
       },
     });
 
-    const games = response.data.games
-    const liveGames = games.filter(game => game.schedule.playedStatus === "LIVE").map(game => `${game.schedule.awayTeam.abbreviation} vs. ${game.schedule.homeTeam.abbreviation}`)
-    const upcomingGames = games.filter(game => game.schedule.playedStatus === "UNPLAYED").map(game => `${game.schedule.awayTeam.abbreviation} vs. ${game.schedule.homeTeam.abbreviation}`)
+    const arrOfMessages = createMessagesForLiveGames(response);
 
-    console.log('Today: ', Object.keys(response.data.games[0]));
-    console.log('live games: ', liveGames);
-    console.log('upcoming games: ', upcomingGames)
+    return arrOfMessages;
   } catch (error) {
-    console.error('Neils error: ', error)
+    console.error('Error within todaysGames function: ', error);
   }
-}
+};
 
-const fetchCurrentScore = (dateAwayHome) => {
-  try {
-    return axios({
-      method: "get",
-      url: `https://api.mysportsfeeds.com/v2.0/pull/nba/2018-2019-regular/games/${dateAwayHome}/boxscore.json`,
-      dataType: 'json',
-      headers: {
-        "Authorization": "Basic " + encryptedAPIKey
-      },
-    }).then(response => {
-      const lastUpdatedOn = response.data.lastUpdatedOn
-      const awayTeam = `${response.data.references.teamReferences[0].city} ${response.data.references.teamReferences[0].name}`
-      const homeTeam = `${response.data.references.teamReferences[1].city} ${response.data.references.teamReferences[1].name}`
-      const currentQuarter = response.data.scoring.currentQuarter
-      const currentIntermission = response.data.scoring.currentIntermission
-      const quarterOrHalftime = currentIntermission === 1 ? 'end of 1st quarter' : currentIntermission === 2 ? 'halftime' : currentIntermission === 3 ? 'end of 3rd quarter' : currentIntermission === 4 ? 'end of regulation' : ''
-      const secondsRemaingingInQuarter = response.data.scoring.currentQuarterSecondsRemaining
-      const awayScoreTotal = response.data.scoring.awayScoreTotal
-      const homeScoreTotal = response.data.scoring.homeScoreTotal
-      const suffix = currentQuarter === 1 ? 'st' : currentQuarter === 2 ? 'nd' : currentQuarter === 3 ? 'rd' : currentQuarter === 4 ? 'th' : ''
-      const currentDisplay = () => {
-        const score = `${awayTeam}: ${awayScoreTotal} - ${homeTeam}: ${homeScoreTotal}`
-        const quarterMessage = `${Math.floor(secondsRemaingingInQuarter/60)}:${secondsRemaingingInQuarter % 60} left in the ${currentQuarter}${suffix}`
-        const intermissionMessage = quarterOrHalftime
-        const timeRemaining = currentQuarter ? quarterMessage : intermissionMessage
-        return `${score} --------- ${timeRemaining}.`
-      }
-
-      // console.log('Last updated on: ', lastUpdatedOn);
-      // console.log(currentDisplay())
-      // console.log('Seconds remaingin in quarter: ', secondsRemaingingInQuarter)
-      // console.log('current quarter: ', response.data.scoring.currentQuarter)
-      // console.log('current intermission: ', response.data.scoring.currentIntermission)
-      // console.log('Played Status: ', response.data.game.playedStatus)
-
-      return currentDisplay();
-    }).then(messageToBeSent => {
-      // console.log('message to be sent: ', messageToBeSent)
-
-      // twilioClient.messages.create({
-      //   to: 8013895313,
-      //   from: 4159802839,
-      //   body: messageToBeSent
-      // })
-      return messageToBeSent
-    })
-
-  } catch (error) {
-    console.error('Neils error: ', error)
-  }
-}
-
-// When this script is running, todaysLiveGames() will run every 15 seconds.
+// When this script is running, fetchCurrentScore() will run every 15 seconds.
 // COULD FIND A WAY TO DYNAMICALLY INPUT TIMES TO RUN DEPENDING ON THE SCHEDULED GAMES THAT DAY.
 // Cron job scheduling examples - https://github.com/kelektiv/node-cron/tree/master/examples
-
 cron.schedule('*/15 * * * * *', () => {
-  console.log('cron ran at ' + new Date())
-  // todaysGames();
-  fetchCurrentScore('20190117-NYK-WAS').then(message => console.log(message));
+  console.log(`****** Cron Job ran at ${new Date()} *******`);
 
-  // console.log('message from func *****: ', message)
-})
+  todaysGames('20190117').then(messagesArr => {
+    messagesArr.forEach(message => {
+      console.log(message);
+      // twilioClient.messages.create({
+      //   to: MY_NUMBER,
+      //   from: TWILIO_NUMBER,
+      //   body: message
+      // })
+    });
+  });
+});
---
+++ utilityFunctions.js
@@ -0,0 +1,86 @@
+const createMessagesForLiveGames = response => {
+
+  // Creates an array of only the live games.
+  const allLiveGames = response.data.games.filter(
+    game => game.schedule.playedStatus === 'LIVE'
+  );
+
+  // Creates a human readable message for each live game.
+  const liveGameMessagesArr = allLiveGames.map(game => {
+    // Uses each team's abbreviations to create a string of their full names.
+    const awayTeamAbb = game.schedule.awayTeam.abbreviation;
+    const homeTeamAbb = game.schedule.homeTeam.abbreviation;
+    const awayTeamFullName = response.data.references.teamReferences.reduce(
+      (name, team) => {
+        if (team.abbreviation === awayTeamAbb) {
+          name = `${team.city} ${team.name}`;
+        }
+        return name;
+      },
+      ''
+    );
+    const homeTeamFullName = response.data.references.teamReferences.reduce(
+      (name, team) => {
+        if (team.abbreviation === homeTeamAbb) {
+          name = `${team.city} ${team.name}`;
+        }
+        return name;
+      },
+      ''
+    );
+    // Determines each team's score.
+    const awayScoreTotal = game.score.awayScoreTotal;
+    const homeScoreTotal = game.score.homeScoreTotal;
+    // Finds the value for the current quarter and/or intermission.
+    const currentQuarter = game.score.currentQuarter;
+    const currentIntermission = game.score.currentIntermission;
+    // Creates a message to display if it's an intermission.
+    const intermissionMessage =
+      currentIntermission === 1
+        ? 'end of 1st quarter'
+        : currentIntermission === 2
+        ? 'halftime'
+        : currentIntermission === 3
+        ? 'end of 3rd quarter'
+        : currentIntermission === 4
+        ? 'end of regulation'
+        : '';
+    // Creates the suffix to display after the appropriate quarter.
+    const suffix =
+      currentQuarter === 1
+        ? 'st'
+        : currentQuarter === 2
+        ? 'nd'
+        : currentQuarter === 3
+        ? 'rd'
+        : currentQuarter === 4
+        ? 'th'
+        : '';
+    // Determines the minutes and seconds to display in mm:ss format depending on how many total seconds remain in the quarter.
+    const totalSecondsRemaingingInQuarter =
+      game.score.currentQuarterSecondsRemaining;
+    const minutesRemaining = Math.floor(totalSecondsRemaingingInQuarter / 60);
+    const secondsRemaining =
+      totalSecondsRemaingingInQuarter % 60 > 9
+        ? totalSecondsRemaingingInQuarter % 60
+        : `0${totalSecondsRemaingingInQuarter % 60}`;
+
+    // Compiles a neat final string using all the above data, depending on whether it's a quarter or an intermission.
+    const compiledMessage = () => {
+      const score = `${awayTeamFullName}: ${awayScoreTotal} - ${homeTeamFullName}: ${homeScoreTotal}`;
+      const quarterMessage = `${minutesRemaining}:${secondsRemaining} left in the ${currentQuarter}${suffix}`;
+      const timeRemaining = currentQuarter
+        ? quarterMessage
+        : intermissionMessage;
+      return `${score} --------- ${timeRemaining}.`;
+    };
+
+    // Returns the compiled message for each game.
+    return compiledMessage();
+  });
+
+  // Returns an array of compiled messages for each live game.
+  return liveGameMessagesArr;
+};
+
+module.exports = createMessagesForLiveGames;
